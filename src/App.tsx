3f2941652f8928a424be430f67f8b52da210f986
JolzyFx Codes <jolzyanth@gmail.com>
2023-05-03 07:25:39 +0100

    track

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import ExpanseFilter from "./Expanse-Tracker/ExpanseFilter";
+import ExpanseForm from "./Expanse-Tracker/ExpanseForm";
+import ExpanseList from "./Expanse-Tracker/ExpanseList";
+import { useState } from "react";
+import categories from "./Expanse-Tracker/categories";
+
+function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState(
+    [
+    { id: 1, description: "Football", amount: 10, category: "Entertainment" },
+    // { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    // { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    // { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]
+  );
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
+  return (
+    <div>
+      <div className="mb-5">
+        <ExpanseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
+      <div className="mb-3">
+        <ExpanseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+
+      <ExpanseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
+  );
+}
+
+export default App;
